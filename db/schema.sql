ab79b760c00d56dbf7edc9bad76cf801812ef18a
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-07-27 18:40:37 -0400

    added create departments to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,10 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+CREATE TABLE departments(
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(50) NOT NULL, 
+    PRIMARY KEY (id)
+)
+
+
--- server.js
+++ server.js
@@ -10,3 +10,24 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// startQuestion Function
+
+function startQuestions() {
+    
+}
+
+
+
+
+
+
+
+
+//   Not Found default response
+app.use((req, res) => {
+    res.status(404).end();
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
